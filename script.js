18c79b417a7929e0cc23c0a926e87fce9bcdab4c
Thomas Ghobril <thomas.ghobril@gmail.com>
2024-03-08 15:30:40 +0100

    update messages

--- script.js
+++ script.js
@@ -21,3 +21,24 @@ function applique(f, tab) {
 
 console.log(applique(fact,[1, 2, 3, 4, 5]));
 console.log(applique(x => x + 1, [1, 2, 3, 4, 5]));
+
+msgs = [
+    { "msg" : "Hello World" },
+    { "msg" : "Blah Blah" },
+    { "msg" : "I love cats" }
+];
+
+function update(msgs) {
+    const ul = document.getElementById('messages');
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+    applique(msg => {
+        const li = document.createElement('li');
+        li.textContent = msg.msg;
+        ul.appendChild(li);
+    }, msgs);
+}
+
+const button = document.getElementById('send');
+button.onclick = () => update(msgs);
