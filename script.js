0583438782fd74fa90cc93d066f8ace190d0a092
Thomas Ghobril <thomas.ghobril@gmail.com>
2024-03-08 15:15:14 +0100

    function applique()

--- script.js
+++ script.js
@@ -8,4 +8,16 @@ function fact(n) {
     return result;
 }
 
-console.log(fact(10))
+console.log(fact(10));
+
+function applique(f, tab) {
+    // return tab.map(f);
+    res = [];
+    tab.forEach(x => {
+        res.push(f(x));
+    });
+    return res;
+}
+
+console.log(applique(fact,[1, 2, 3, 4, 5]));
+console.log(applique(x => x + 1, [1, 2, 3, 4, 5]));
